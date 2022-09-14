a79436dc61ceab9410fe15e26467419a609a65b7
Abhishek Rajput <rajput420avi@gmail.com>
2022-09-14 16:47:11 +0530

    modify problem1.go

--- problem1.go
+++ problem1.go
@@ -3,7 +3,7 @@ package EulerProject
 
 //3 U 5= either of 3 and either of 5
 
-func problem1(number int) int {
+func Problem1(number int) int {
 	var sum int = 0
 	for i := 1; i <= number; i++ {
 		if i%3 == 0 || i%5 == 0 {
